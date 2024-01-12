fa391db6739ddbd381560e494d28950a58295365
NicolasCastela <nickcastela2022@hotmail.com>
2024-01-12 10:06:56 -0300

    add middlewares

--- modules/express.js
+++ modules/express.js
@@ -1,38 +1,64 @@
-const express = require('express')
+const express = require("express");
 const port = 8080;
-const app = express()
-app.use(express.json())
-const UserModel = require('../src/models/user.model')
+const app = express();
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`Request-Type: ${req.method} `);
+  console.log(`Content-Type: ${req.headers["content-type"]} `);
+  console.log(`Data: ${new Date()}`);
+  next();
+});
+const UserModel = require("../src/models/user.model");
 
-app.get('/', (req, res) => {
-  res.status(200).send("hello")
-})
-app.get('/users', (_, res) => {
-  const users = [
-    {
-      name: 'Nicolas Castela',
-      email: 'nickcastela@hotmail.com'
-    },
-    {
-      name: 'Marcel Gayzin',
-      email: 'marcelgay@hotmail.com'
-    }
-  ];
-  JSON.stringify(users)
-  res.status(200).send(users)
-
+app.get("/", (req, res) => {
+  res.status(200).send("hello");
+});
+app.get("/users", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.status(200).json(users);
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
 });
 
-app.post('/users', async (req, res) => {
+app.post("/users", async (req, res) => {
   try {
-  const user = UserModel.create(req.body);
-  res.status(201).json(user);
+    const user = UserModel.create(req.body);
+    res.status(201).json(user);
   } catch (error) {
-    res.status(500).send(error.message)
+    res.status(500).send(error.message);
   }
-})
+});
 
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findById(id);
+    return res.status(200).json(user);
+  } catch (e) {
+    console.log(e);
+  }
+});
 
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
 
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndDelete(id);
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
 
-app.listen(port, () => console.log(`running in ${port}`))
+app.listen(port, () => console.log(`Running in ${port}`));
--- src/database/connect.js
+++ src/database/connect.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(`mongodb+srv://nickcastela:${process.env.MONGODB_PASSWORD}@nicolas.r6dwduk.mongodb.net/?retryWrites=true&w=majority`),
-      console.log('conectado com sucesso')
-      }catch (error) {
+    await mongoose.connect(
+      `mongodb+srv://nickcastela:${process.env.MONGODB_PASSWORD}@nicolas.r6dwduk.mongodb.net/?retryWrites=true&w=majority`
+    ),
+      console.log("Api funcionando corretamente.");
+  } catch (error) {
     console.error(error);
-    console.log('Ocorreu um erro ao conectar ao banco de dados MONGODB')
+    console.log("Ocorreu um erro ao conectar ao banco de dados MONGODB");
   }
-}
+};
 
 module.exports = connectToDatabase;
